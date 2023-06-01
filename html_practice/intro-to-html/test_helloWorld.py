a5763debf4f724daadc2d58d0477fcdc01a65c57
Michael-Ude <ude00003@umn.edu>
2023-06-01 16:23:32 +0200

    addiing a slightly more complicated function for the test (correct)

--- html_practice/intro-to-html/test_helloWorld.py
+++ html_practice/intro-to-html/test_helloWorld.py
@@ -1,4 +1,22 @@
-def test_func():
-    #computes 2+2
-    assert 2+2 == 4
+# def test_func():
+#     #computes 2+2
+#     assert 2+2 == 4
     
+def calculate_fibonacci(n):
+    """Calculates the nth Fibonacci value.
+
+    Args:
+        n (int): The index of the desired Fibonacci value.
+
+    Returns:
+        The nth Fibonacci value.
+    """
+    # Base cases
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+
+    # Recursive case
+    else:
+        return calculate_fibonacci(n-1) + calculate_fibonacci(n-2)
